95341cc31c6748e944da4bf32dbfb67209f2e102
Aboodeh <122124939+Aboooodeh@users.noreply.github.com>
2023-01-21 20:07:00 +0200

    Added getComputerChoice function

--- jvs.js
+++ jvs.js
@@ -1,7 +1,28 @@
 function getComputerChoice() {
-  let choices = ["Rock", "Paper", "Scissors"];
+  let choices = ["rock", "paper", "scissors"];
 
   let index = Math.floor(Math.random() * 3);
 
-  return answers[index];
+  return choices[index];
 }
+
+function round(ps, cs) {
+  let result;
+  ps = ps.toLowerCase();
+  if (ps === cs) result = "It's a tie";
+  else {
+    if (ps === "rock") {
+      if (cs === "scissors") result = "You Win, Rock beats Scissors";
+      if (cs === "paper") result = "You Lose, Paper beats Rock";
+    } else if (ps === "scissors") {
+      if (cs === "paper") result = "You Win, Scissors beats Paper";
+      if (cs === "rock") result = "You Lose, Rock beats Scissors";
+    } else if (ps === "paper") {
+      if (cs === "scissors") result = "You Lose, Scissors beats Paper";
+      if (cs === "rock") result = "You Win, Paper beats Rock";
+    } else result = "errorrrrrrr";
+  }
+  return result;
+}
+let g = "ROCK";
+console.log(round(g, getComputerChoice()));
