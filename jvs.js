2c50de5cb1ad4ee1b7f7fc79f08ecb3d46bf45f3
Aboodeh <122124939+Aboooodeh@users.noreply.github.com>
2023-01-21 21:04:10 +0200

    Added Game function

--- jvs.js
+++ jvs.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerChoice() {         //This function is to get computer choice
   let choices = ["rock", "paper", "scissors"];
 
   let index = Math.floor(Math.random() * 3);
@@ -6,23 +6,42 @@ function getComputerChoice() {
   return choices[index];
 }
 
-function round(ps, cs) {
-  let result;
-  ps = ps.toLowerCase();
-  if (ps === cs) result = "It's a tie";
+function round(hc, cc) {                        //This function is to determine the winner
+  let result;                                   // h = Human , c = Computer
+  hc = hc.toLowerCase();
+  if (hc === cc) result = "t";
   else {
-    if (ps === "rock") {
-      if (cs === "scissors") result = "You Win, Rock beats Scissors";
-      if (cs === "paper") result = "You Lose, Paper beats Rock";
-    } else if (ps === "scissors") {
-      if (cs === "paper") result = "You Win, Scissors beats Paper";
-      if (cs === "rock") result = "You Lose, Rock beats Scissors";
-    } else if (ps === "paper") {
-      if (cs === "scissors") result = "You Lose, Scissors beats Paper";
-      if (cs === "rock") result = "You Win, Paper beats Rock";
-    } else result = "errorrrrrrr";
+    if (hc === "rock") {
+      if (cc === "scissors") result = "h";
+      if (cc === "paper") result = "c";
+    } else if (hc === "scissors") {
+      if (cc === "paper") result = "h";
+      if (cc === "rock") result = "c";
+    } else if (hc === "paper") {
+      if (cc === "scissors") result = "c";
+      if (cc === "rock") result = "h";
+    } else result = "e";
   }
   return result;
 }
-let g = "ROCK";
-console.log(round(g, getComputerChoice()));
+
+function game() {                                                     //This function is to start a game;
+  let hs = 0;
+  let cs = 0;
+  for (let i = 0; i < 5; i++) {
+    let result = round(
+      prompt("enter Rock, Paper, Scissors"),
+      getComputerChoice()
+    );
+    if (result === "h") hs++;
+    else if (result === "t") cs++;
+    else if (result === "e") {
+      console.log("input error, try again");                 // this to cancel the round if the input is invalid
+      i--;
+    }
+    console.log(`Your score is ${hs}, Computer score ${cs}`);
+    if (hs >= 3 || cs >= 3) break;
+  }
+  if (hs > cs) console.log("You Win");
+  else console.log("You Lose");
+}
