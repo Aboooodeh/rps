978eea1bdd43fa0a4b01c746c3695fdba519ed0c
Aboodeh <122124939+Aboooodeh@users.noreply.github.com>
2023-01-21 18:20:53 +0200

    Started project
    edited README file
    created a skeleton html
    created a javascript file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rps
+This project is part of the odin project course
+
+it will test javascript skills obtained
---
+++ jvs.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+  let choices = ["Rock", "Paper", "Scissors"];
+
+  let index = Math.floor(Math.random() * 3);
+
+  return answers[index];
+}
